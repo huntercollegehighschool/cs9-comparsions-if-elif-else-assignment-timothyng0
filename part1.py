7131f40d711f77309b4bbb9f66f79d153af7730d
Timothy Ng <94186690+timothyng0@users.noreply.github.com>
2021-12-06 21:48:51 +0000

    I added line 23 which is the same as line 22 and 18.

--- part1.py
+++ part1.py
@@ -20,7 +20,7 @@ number = int(input("Enter a number: "))
 smallest = number
 
 number = int(input("Enter another number: "))
-
+number = int(input("Enter another number: "))
 if number < smallest:
   smallest = number
   
